b104de072d9dba7b43bf3a9b59c7303aca791a06
HuzaifaKhanNiazi <0044huzaifakhanniazi@gmail.com>
2026-07-30 12:43:52 +0500

    Add new files

--- App Backend/App/config.py
+++ App Backend/App/config.py
@@ -1,4 +1,9 @@
 from dotenv import load_dotenv
+from pathlib import Path
 import os
-load_dotenv()
-GROQ_API_KEY=os.getenv('GROQ_API_KEY')
+
+env_path = Path(__file__).parent / ".env"
+load_dotenv(env_path)
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
